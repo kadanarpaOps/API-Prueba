2d9787663f60d004a6308e3a350a5171cd221f74
daisy-bait <kadanarpa@gmail.com>
2026-09-18 00:33:45 -0500

    [LEC-10] feat: probando

---
+++ bootstrap/src/main/java/top/prueba/kadanarpa/api_prueba/controller/PruebaController.java
@@ -0,0 +1,16 @@
+package top.prueba.kadanarpa.api_prueba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/prueba")
+public class PruebaController {
+
+    @GetMapping("/saludar")
+    public String saludar(){
+        return "Hola Mundo";
+    }
+
+}
